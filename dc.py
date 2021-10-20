52fb6f824719fd6b590adc72c0822da5c3568b5b
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-10-20 20:36:01 +0000

    start dc code, change servo max

--- dc.py
+++ dc.py
@@ -1,1 +1,12 @@
-#need a pwm signal 
+#need a pwm signal to ramp motor from max speed to still in 2 sec
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+pin = 26
+GPIO.setup(pin, GPIO.OUT)
+
+pwm = GPIO.PWM(pin, 50) # PWM object at 50 Hz (20 ms period)
+pwm.start(0)
+
